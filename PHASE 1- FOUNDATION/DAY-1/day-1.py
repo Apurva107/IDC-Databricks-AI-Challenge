2ce69b4acd291ce2293f2edfa1f7ab5f728fa998
Apurva <apurvapatil71@gmail.com>
2026-01-09 11:31:18 +0100

    Update and rename day-1 to day-1.py

--- PHASE 1- FOUNDATION/DAY-1/day-1.py
+++ PHASE 1- FOUNDATION/DAY-1/day-1.py
@@ -1,4 +1,4 @@
-# Databricks notebook source
+.# Databricks notebook source
 # Create simple DataFrame
 data = [("iPhone", 999), ("Samsung", 799), ("MacBook", 1299)]
 df = spark.createDataFrame(data, ["product", "price"])
